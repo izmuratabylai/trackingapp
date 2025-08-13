06b9e8e62430753a6abb67d2fa90a2da744ec5dc
Izmurat Abylay <81766411+izmuratabylai@users.noreply.github.com>
2025-08-13 22:13:27 +0500

    Update globalContext.js
    added a api string base url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:8080/api/v1/";
+const BASE_URL = "https://trackingapp-ycml.onrender.com/api/v1/";
 
 
 const GlobalContext = React.createContext()
